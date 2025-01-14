5044beca6c9ec1b3cbba0a94bbbddf4f1183b760
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2025-01-14 16:37:24 +0530

    change routes for all farmers

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -13,6 +13,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(isAuthenticated, logoutUser);
-router.get("/farmers", isAuthenticated, getAllFarmers);
+router.route("/farmers").get(isAuthenticated, getAllFarmers);
 
 export default router;
